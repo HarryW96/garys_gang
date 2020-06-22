209525f06bc0f50bb8044b641ce735c5b47f6e34
Tom Chate <up774061@myport.ac.uk>
2020-06-22 17:08:03 +0100

    Trash

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,15 @@ const valorantAPI = require('../valorant-api');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/valorant', function(req, res, next) {
+router.get('/valorant', function (req, res, next) {
   console.log("valorant from index router");
-  storedPlayers = valorantAPI.getallplayers();
-  console.log(storedPlayers);
-  res.render('valorant', { title: 'Valorant', players: storedPlayers });
+  newplayers = await valorantAPI.getallplayers();
+  res.render('valorant', { title: 'Valorant', players: newplayers })
+
 });
 
 module.exports = router;
--- valorant-api.js
+++ valorant-api.js
@@ -2,35 +2,43 @@
 var express = require('express');
 var { connection, Request } = require('tedious');
 var bodyParser = require('body-parser');
-var http = require('http'); 
+var http = require('http');
 const sqlSettings = require('./sql');
+const sql = require('mssql')
 var dbPlayers = [];
 
-function getallplayers(){
+async function getallplayers() {
   console.log("getallplayers")
   connection = sqlSettings.returnConnection();
+  dbPlayers = [];
 
   connection.on("connect", err => {
     if (err) {
       console.error(err.message);
     } else {
       const request = new Request(
-      "SELECT discordName as PlayerName FROM Player",
-      (err, rowCount) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log(`${rowCount} row(s) returned`);
-      }
-      ); 
-    request.on("row", columns => {
-      columns.forEach(column => {
-        dbPlayers.push(column.value);
+        "SELECT discordName as PlayerName FROM Player",
+        (err, rowCount) => {
+          if (err) {
+            console.error(err.message);
+          } else {
+            //it works
+            console.log(`${rowCount} row(s) returned`);
+            return dbPlayers;
+          }
+        }
+      )
+      connection.execSql(request);
+
+      request.on("row", columns => {
+        columns.forEach(column => {
+          dbPlayers.push(column.value);
+        });
       });
-    });
-    connection.execSql(request);
-    } 
+    }
   })
+  
 }
+
 
 exports.getallplayers = getallplayers;
